399b86a8c798f8f89899081a4129c362d222529d
Crowdbotics <engineering@crowdbotics.com>
2023-05-05 02:45:54 +0000

    Update store.

--- store/feedtestappAPI/api.js
+++ store/feedtestappAPI/api.js
@@ -3,6 +3,12 @@ const feedtestappAPI = axios.create({
   baseURL: "https://feedtestapp-40468-prod.herokuapp.com",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
+function api_v1_post_list(payload) {
+  return feedtestappAPI.get(`/api/v1/post/`)
+}
+function api_v1_post_create(payload) {
+  return feedtestappAPI.post(`/api/v1/post/`, payload.data)
+}
 function api_v1_login_create(payload) {
   return feedtestappAPI.post(`/api/v1/login/`, payload.data)
 }
@@ -25,6 +31,18 @@ function api_docs_schema_retrieve(payload) {
 }
 function rest_auth_login_create(payload) {
   return feedtestappAPI.post(`/rest-auth/login/`, payload.data)
+}
+function api_v1_post_retrieve(payload) {
+  return feedtestappAPI.get(`/api/v1/post/${payload.id}/`)
+}
+function api_v1_post_update(payload) {
+  return feedtestappAPI.put(`/api/v1/post/${payload.id}/`, payload.data)
+}
+function api_v1_post_partial_update(payload) {
+  return feedtestappAPI.patch(`/api/v1/post/${payload.id}/`, payload.data)
+}
+function api_v1_post_destroy(payload) {
+  return feedtestappAPI.delete(`/api/v1/post/${payload.id}/`)
 }
 function rest_auth_logout_retrieve(payload) {
   return feedtestappAPI.get(`/rest-auth/logout/`)
@@ -51,6 +69,8 @@ function rest_auth_registration_verify_email_create(payload) {
   )
 }
 export const apiService = {
+  api_v1_post_list,
+  api_v1_post_create,
   api_v1_login_create,
   api_v1_signup_create,
   rest_auth_user_retrieve,
@@ -58,6 +78,10 @@ export const apiService = {
   rest_auth_user_partial_update,
   api_docs_schema_retrieve,
   rest_auth_login_create,
+  api_v1_post_retrieve,
+  api_v1_post_update,
+  api_v1_post_partial_update,
+  api_v1_post_destroy,
   rest_auth_logout_retrieve,
   rest_auth_logout_create,
   rest_auth_registration_create,
