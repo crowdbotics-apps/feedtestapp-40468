1d72acdfbbc4946dc00116aaf4f1b846479d88ad
Carlos Paparoni <carlos.paparoni.fsl@crowdbotics.com>
2023-05-04 22:32:36 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "feedtestapp-40468",
-  url: "https://feedtestapp-40468.botics.co",
-  api: "https://feedtestapp-40468.botics.co/api/v1"
-}
+  "name": "feedtestapp-40468",
+  "url": "https://feedtestapp-40468.botics.co",
+  "api": "https://feedtestapp-40468.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
