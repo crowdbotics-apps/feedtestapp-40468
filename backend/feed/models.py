02462394b29e376eca60fe3651a216643a5bae4e
Carlos Paparoni <130712450+cpaparoni-crowdbotics@users.noreply.github.com>
2023-05-04 22:49:53 -0400

    Update models.py
    Add models for a simple feed

--- backend/feed/models.py
+++ backend/feed/models.py
@@ -1,12 +1,34 @@
 from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User
+
 class Post(models.Model):
     'Generated Model'
-    caption = models.CharField(max_length=2200,)
-    media = models.CharField(max_length=256,)
-    created_date = models.DateTimeField(auto_now_add=True,)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    caption = models.CharField(max_length=2200)
+    image = models.ImageField(upload_to='posts/')
+    created_date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created_date']
+
 class Like(models.Model):
     'Generated Model'
-    created_date = models.DateTimeField(auto_now_add=True,)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ('user', 'post')
+
+class Comment(models.Model):
+    'Generated Model'
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.CharField(max_length=1000)
+    created_date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created_date']
 
 # Create your models here.
