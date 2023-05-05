65cbf421211a6c54f7d01cfd3cc92bdc92e8c54c
Carlos Paparoni <carlos.paparoni.fsl@crowdbotics.com>
2023-05-05 00:35:28 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ export const globalOptions = {
   "name": "feedtestapp-40468",
   "url": "https://feedtestapp-40468.botics.co",
   "api": "https://feedtestapp-40468.botics.co/api/v1",
-  "initialRoute": "Untitled1"
+  "initialRoute": "splash"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
